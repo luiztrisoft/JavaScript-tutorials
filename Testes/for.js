e6f20ae05307085e7ce480c710f87b738ea25ea1
luiz.ricardo <luiz.ricardo@groupsoftware.com.br>
2021-05-12 17:19:01 -0300

    Novos testes

--- Testes/for.js
+++ Testes/for.js
@@ -1,10 +1,12 @@
-//for in 
+//for in (objeto)
 var objeto = {nome: "Luiz", idade: 34, sobrenome: "Alberto"}
 for(var prop in objeto){
   console.log("Atributo " + prop + " = " + objeto[prop])
 }
+
 console.log('-----------------------------------------')
-//for of
+
+//for of (array)
 var dias = ["Sabado", "Domingo"]
 for (var elemento of dias){
   console.log("Elemento do array: " + elemento)
