0368ff3df0d3f65f04caf09879c88b11a23da6e9
luiz.ricardo <luiz.ricardo@groupsoftware.com.br>
2021-05-17 10:43:18 -0300

    every e some

--- Testes/Every e Some.js
+++ Testes/Every e Some.js
@@ -3,12 +3,12 @@ function isBigEnough(currentValue, index, arr) {
   return currentValue >= 10;
 }
 /*
-  Verifica se cada elemento passa no teste
+* Verifica se cada elemento passa no teste
 */
 console.log([12, 5, 8, 130, 44].every(isBigEnough));   // false (Um ou mais elementos não passam de 10)
 console.log([12, 54, 18, 130, 44].every(isBigEnough)); // true (Todos os elementos passam de 10)
 
 /*
-Verifica se ao menos um elemento passa no teste
+* Verifica se ao menos um elemento passa no teste
 */
 console.log([1, 5, 8, 130, 4].some(isBigEnough));   // true (Ao menos um elemento passa de 10)
