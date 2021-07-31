de70d25f0ff8b0858b8f07946a93cea043b5c267
user <luiz.trisoft@gmail.com>
2021-07-31 13:13:20 -0300

    MJS

---
+++ MJS/index.js
@@ -0,0 +1,11 @@
+var exec = 'node script.js'
+console.log('=>', exec)
+console.log('Se o arquivo for index.js podemos chamar "node *" ou "node pastaDoIndex/" ')
+
+console.log(typeof null)
+console.log(typeof '')
+console.log(typeof 5)
+console.log(typeof true)
+
+
+  
