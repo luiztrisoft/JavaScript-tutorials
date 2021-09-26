5060b9130105d435cd00a517ad645ee3d9f0afea
Luiz A. R. da Silva <luiz.trisoft@gmail.com>
2021-09-26 11:04:55 -0300

    Update valor monetario.js

--- Utils P&D/valor monetario.js
+++ Utils P&D/valor monetario.js
@@ -3,5 +3,5 @@ let atual = 500000.00
 let valor1 = atual.toLocaleString('pt-br', {style:'currency', currency:'BRL'});
 let valor2 = atual.toLocaleString('pt-br', {minimumFractionDigits: 2});
 
-//Saída com R$ R$ 500.000,00
-//Saída sem R$500.000,00
+//Saída com R$ - R$ 500.000,00
+//Saída sem R$ -    500.000,00
